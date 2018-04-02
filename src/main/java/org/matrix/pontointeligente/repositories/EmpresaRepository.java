a65ad8b5ba76abbaa435c1af11788caa7f8f6de4
Your Name <you@example.com>
2018-04-01 23:09:54 -0300

    repositories

---
+++ src/main/java/org/matrix/pontointeligente/repositories/EmpresaRepository.java
@@ -0,0 +1,14 @@
+package org.matrix.pontointeligente.repositories;
+
+
+
+import org.matrix.pontointeligente.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
---
+++ src/main/java/org/matrix/pontointeligente/repositories/FuncionarioRepository.java
@@ -0,0 +1,15 @@
+package org.matrix.pontointeligente.repositories;
+
+import org.matrix.pontointeligente.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+}
